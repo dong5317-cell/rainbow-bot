65e7e55ce919e6ab9cc6ccc39836f9396ec6297a
dong5317 <dong5317@gmail.com>
2026-07-10 23:13:39 +0900

    Update rainbow bot

--- index.js
+++ index.js
@@ -4,17 +4,17 @@ const client = new Client({
     intents: [GatewayIntentBits.Guilds]
 });
 
-const TOKEN = process.env.TOKEN;
-const ROLE_ID = process.env.ROLE_ID;
+const TOKEN = process.env.TOKEN; 
+const ROLE_ID = process.env.ROLE_ID;;
 
 const colors = [
-    0xff0000, // 빨강
-    0xff7f00, // 주황
-    0xffff00, // 노랑
-    0x00ff00, // 초록
-    0x00ffff, // 하늘
-    0x0000ff, // 파랑
-    0x8b00ff  // 보라
+    0xff0000,
+    0xff7f00,
+    0xffff00,
+    0x00ff00,
+    0x0000ff,
+    0x4b0082,
+    0x9400d3
 ];
 
 client.once("ready", () => {
@@ -26,15 +26,12 @@ client.once("ready", () => {
         const guild = client.guilds.cache.first();
         const role = guild.roles.cache.get(ROLE_ID);
 
+        if (!role) return;
+
         await role.setColor(colors[i]);
 
-        i++;
-
-        if (i >= colors.length) {
-            i = 0;
-        }
-
-    }, 3000);
+        i = (i + 1) % colors.length;
+    }, 1000);
 });
 
 client.login(TOKEN);
