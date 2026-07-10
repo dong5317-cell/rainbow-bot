431f017575690cff15ade0435480997419258f5d
dong5317 <dong5317@gmail.com>
2026-07-10 22:34:24 +0900

    Update rainbow bot

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-const { Client, GatewayIntentBits } = require("discord.js");
+onst { Client, GatewayIntentBits } = require("discord.js");
 
 const client = new Client({
     intents: [GatewayIntentBits.Guilds]
@@ -7,81 +7,31 @@ const client = new Client({
 const TOKEN = process.env.TOKEN;
 const ROLE_ID = process.env.ROLE_ID;
 
-// 무지개 색상 (42단계)
 const colors = [
     0xff0000,
-    0xff2200,
-    0xff4400,
-    0xff6600,
-    0xff8800,
-    0xffaa00,
-    0xffcc00,
-    0xffee00,
+    0xff7f00,
     0xffff00,
-
-    0xddff00,
-    0xbbff00,
-    0x99ff00,
-    0x77ff00,
-    0x55ff00,
-    0x33ff00,
     0x00ff00,
-
-    0x00ff33,
-    0x00ff66,
-    0x00ff99,
-    0x00ffcc,
-    0x00ffff,
-
-    0x00ddff,
-    0x00bbff,
-    0x0099ff,
-    0x0077ff,
-    0x0055ff,
-    0x0033ff,
     0x0000ff,
-
-    0x2200ff,
-    0x4400ff,
-    0x6600ff,
-    0x8800ff,
-    0xaa00ff,
-    0xcc00ff,
-    0xee00ff,
-    0xff00ff,
-
-    0xff00cc,
-    0xff0099,
-    0xff0066,
-    0xff0033,
-    0xff0011,
-    0xff0000
+    0x4b0082,
+    0x9400d3
 ];
 
-client.once("clientReady", () => {
-    console.log(`${client.user.tag} 실행됨!`);
+client.once("ready", () => {
+    console.log(`${client.user.tag} 실행됨`);
 
     let i = 0;
 
-setInterval(async () => {
-    try {
+    setInterval(async () => {
         const guild = client.guilds.cache.first();
-        if (!guild) return;
+        const role = guild.roles.cache.get(ROLE_ID);
 
-        // 매번 최신 역할 정보 가져오기
-        const role = await guild.roles.fetch(ROLE_ID);
         if (!role) return;
-
-        console.log(`색 변경: ${i}`);
 
         await role.setColor(colors[i]);
 
         i = (i + 1) % colors.length;
-
-    } catch (err) {
-        console.error("오류:", err);
-    }
-}, 1000); // 1초마다 변경
+    }, 500);
 });
 
 client.login(TOKEN);
