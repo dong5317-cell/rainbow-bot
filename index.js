6bbd252387fc206abb6d70c0a2e1f780a9d20b85
dong5317 <dong5317@gmail.com>
2026-07-11 18:01:09 +0900

    Update to discord.js v14 role edit

--- index.js
+++ index.js
@@ -53,7 +53,7 @@ client.once("clientReady", () => {
             index = (index + 1) % colors.length;
         }
 
-        setTimeout(changeColor, 1000);
+        setTimeout(changeColor, 3000);
     }
 
     changeColor();
