364ee9e953d003fe9e03528724180a9183d4736d
dong5317 <dong5317@gmail.com>
2026-07-11 18:29:19 +0900

    Update to discord.js v14 role edit

--- index.js
+++ index.js
@@ -9,57 +9,43 @@ const ROLE_ID = process.env.ROLE_ID;
 
 const colors = [
     0xff0000,
-    0xff7f00,
-    0xffff00,
     0x00ff00,
-    0x0000ff,
-    0x4b0082,
-    0x9400d3
+    0x0000ff
 ];
 
 client.once("clientReady", () => {
     console.log(`${client.user.tag} is online`);
 
     let index = 0;
-    let changing = false;
 
     async function changeColor() {
-
-        if (changing) {
-            console.log("Previous change still running");
-            return;
-        }
-
-        changing = true;
-
         try {
             const guild = client.guilds.cache.first();
+
+            if (!guild) {
+                console.log("Guild not found");
+                return;
+            }
 
             const role = guild.roles.cache.get(ROLE_ID);
 
             if (!role) {
                 console.log("Role not found");
-                changing = false;
                 return;
             }
 
             console.log("Trying color:", index);
 
-            role.setColor(colors[index])
-                .then(() => {
-                    console.log("Color changed:", index);
-                })
-                .catch(err => {
-                    console.log("Color failed:", err.message);
-                });
+            await role.setColor(colors[index]);
+
+            console.log("Color changed:", index);
 
             index = (index + 1) % colors.length;
 
-        } catch (err) {
-            console.log("Error:", err.message);
+        } catch (error) {
+            console.log("Color failed:", index, error.message);
+            index = (index + 1) % colors.length;
         }
-
-        changing = false;
 
         setTimeout(changeColor, 10000);
     }
