2e99cd6d3a4f53fe3e485b164843b896e4c03d2c
dong5317 <dong5317@gmail.com>
2026-07-11 19:48:57 +0900

    Update to discord.js v14 role edit

--- index.js
+++ index.js
@@ -10,13 +10,7 @@ const TOKEN = process.env.TOKEN;
 const ROLE_ID = process.env.ROLE_ID;
 
 const colors = [
-    0xff0000,
-    0xff7f00,
-    0xffff00,
-    0x00ff00,
-    0x0000ff,
-    0x4b0082,
-    0x9400d3
+    0xffff00
 ];
 
 client.once("clientReady", async () => {
